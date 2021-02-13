04aa5248e1f2616fc8964b6d8887f9fda610e28a
AnjaliSharma2021 <77712411+AnjaliSharma2021@users.noreply.github.com>
2021-02-13 18:36:07 -0500

    Update urls.py

--- code/glucocheck/homepage/urls.py
+++ code/glucocheck/homepage/urls.py
@@ -6,7 +6,7 @@ from .import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('signup/', views.signup, name = 'signup'),
-    path('login/', views.login),
+    path('login/', views.login, name = 'login'),
     
 ]
     
