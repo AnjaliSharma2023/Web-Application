616cc44a321d0d8a83ca7a79f87b123cb910b4b6
krmcgregor1 <krmcgregor1@my.waketech.edu>
2021-02-12 23:56:41 -0500

    Fixed a filename typo and added potential links to the nav bar for future use

--- code/glucocheck/homepage/views.py
+++ code/glucocheck/homepage/views.py
@@ -4,9 +4,11 @@ from django.shortcuts import render
 def homepage(request):
     ''' Snippet of the code for inputting the user name into the template
     if request.user.is_authenticated:
-        context = {'username': request.user.name} # or however you reference the user name
+        context = {'username': f'Welcome {request.user.name}!', 'active': 'Home'} # or however you reference the user name
     else:
-        context = {'username': 'Sign-In/Up'}
+        context = {'username': 'Sign-In/Up', 'active': 'Home'}
+        
+    The 'activate' context item represents which navbar is selected and therefore should be coloured differently
     '''
     context = {'username': 'Sign-In/Up',
                'active': 'Home'}
