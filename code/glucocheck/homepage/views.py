4096910677d489407f2ce552868a5c43f8391fc7
krmcgregor1 <krmcgregor1@my.waketech.edu>
2021-02-12 22:06:46 -0500

    Added Django implementation for the homepage ('localhost:8000/'). Moved .html and .css files into '/static/homepage/' and '/template/homepage/' subfolders.

--- code/glucocheck/homepage/views.py
+++ code/glucocheck/homepage/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    ''' Snippet of the code for inputting the user name into the template
+    if request.user.is_authenticated:
+        context = {'username': request.user.name} # or however you reference the user name
+    else:
+        context = {'username': 'Sign-In/Up'}
+    '''
+    context = {'username': 'Sign-In/Up'}
+    return render(request, 'homepage/homepage.html', context)
