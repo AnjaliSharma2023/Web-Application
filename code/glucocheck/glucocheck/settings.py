1ffaae9ca6a9a57621eac995ae3f44896b341ba7
AnjaliSharma2021 <ar_anjali_sharma@yahoo.com>
2021-02-13 10:47:12 -0500

    update

--- code/glucocheck/glucocheck/settings.py
+++ code/glucocheck/glucocheck/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'homepage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'homepage',
+    
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'glucocheck.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- code/glucocheck/homepage/views.py
+++ code/glucocheck/homepage/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from homepage.forms import SignupForm
+from django.contrib import messages
 
 # Create your views here.
+
+def home(request):
+    
+    return render(request,'homepage/home.html')
+
+def signup(request):
+
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user.first_name = cleaned_data['first_name']
+            user.last_name = cleaned_data['last_name']
+            user.email = cleaned_data['email']
+            user.country = cleaned_data['country']
+            messages.success(request,'Account created successfully')
+            return redirect('login')
+    else: 
+        form = SignupForm()
+
+    return render(request,'homepage/signup.html', {'form': form})
+
+
+def login(request):
+    
+    return render(request,'homepage/login.html')
