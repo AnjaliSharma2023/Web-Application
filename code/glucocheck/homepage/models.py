d1bc11cbe1a71f4cf75ae5d071f5ae107273793d
AnjaliSharma2021 <ar_anjali_sharma@yahoo.com>
2021-02-14 16:13:47 -0500

    changes

--- code/glucocheck/homepage/models.py
+++ code/glucocheck/homepage/models.py
@@ -5,5 +5,10 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User,on_delete= models.CASCADE)
-    city = models.CharField(max_length =100,default='')
-    phone = models.IntegerField(default = 0)
+    email = models.CharField(max_length=200,null =True)
+    first_name = models.CharField(max_length=200, null=True)
+    last_name = models.CharField(max_length=200, null=True)
+
+
+    def __str__(self):
+        return self.first_name
