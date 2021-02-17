d90996a072aef6997e9a4dfc3e28fa00caef7304
krmcgregor1 <krmcgregor1@my.waketech.edu>
2021-02-17 12:16:50 -0500

    Added label attribute and placeholder widget back to forms

--- code/glucocheck/homepage/forms.py
+++ code/glucocheck/homepage/forms.py
@@ -4,11 +4,17 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import UserProfile
 
 class SignupForm(UserCreationForm):
-
+    username = forms.CharField(required =True, widget=forms.TextInput(attrs={'placeholder':'Username'}), label='username.svg')
+    email=forms.EmailField(required =True, widget=forms.TextInput(attrs={'placeholder':'Email'}), label='username.svg')
+    birth_date = forms.DateField(widget = forms.SelectDateWidget(attrs={'placeholder':'Birth Date'}), label='username.svg')
+    state = forms.CharField(max_length = 20,required =True, widget=forms.TextInput(attrs={'placeholder':'State'}), label='username.svg')
+    password1 = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder':'Password'}), label='username.svg')
+    password2 = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder':'Password Confirmation'}), label='username.svg')
+    '''
     email=forms.EmailField(required =True)
     birth_date = forms.DateField(label ='Date of birth',help_text='Required. Format: MM-DD-YYYY')
     state = forms.CharField(max_length = 20)
- 
+    '''
     class Meta:
         model = User
         fields = (
