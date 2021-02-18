4805be40b4de787cd7dcc36d30603edac7cbebb7
AnjaliSharma2021 <ar_anjali_sharma@yahoo.com>
2021-02-18 17:11:23 -0500

    changes

--- code/glucocheck/homepage/forms.py
+++ code/glucocheck/homepage/forms.py
@@ -2,22 +2,22 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import UserProfile
-#from django.contrib.localflavor.us.us_states import STATE_CHOICES
-
+from datetime import date
 
 from localflavor.us.forms import USStateSelect
 
-# from django.contrib.localflavor.us.models import USStateField
+
 class SignupForm(UserCreationForm):
     username = forms.CharField(required =True, widget=forms.TextInput(attrs={'placeholder':'Username'}), label='user.svg')
     email=forms.EmailField(required =True, widget=forms.TextInput(attrs={'placeholder':'Email'}), label='envelope.svg')
-    #birth_date = forms.DateField(widget=forms.SelectDateWidget)
+    
     birth_date = forms.DateField(widget = forms.SelectDateWidget(attrs={'placeholder':'Birth Date'}), label='calendar-alt.svg')
-    #state = USStateField(choices = STATE_CHOICES)
+    
     state = forms.CharField(required =True, widget=USStateSelect(attrs={'placeholder':'State'}), label='map-marker-alt.svg')
     #state = USStateSelect(required =True, widget=forms.TextInput(attrs={'placeholder':'State'}), label='map-marker-alt.svg')
     password1 = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder':'Password'}), label='key.svg')
     password2 = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder':'Password Confirmation'}), label='key.svg')
+    #check = forms.BooleanField(required =True)
     '''
     email=forms.EmailField(required =True)
     birth_date = forms.DateField(input_formats=['%d/%m/%Y'])
@@ -34,12 +34,20 @@ class SignupForm(UserCreationForm):
             'state',
         ) 
 
+    def clean_birth_date(self):
+        birth_date = self.cleaned_data['birth_date']
+        today = date.today()
+        if (birth_date.year + 18, birth_date.month, birth_date.day) > (today.year, today.month, today.day):
+            raise forms.ValidationError('Must be at least 18 years old to register')
+        return birth_date
+
     def save(self, commit =True):
         user = super().save(commit= False)
         
         user.email = self.cleaned_data['email']
-        user.birth_date = self.cleaned_data['birth_date']
-        user.set_adult()
+        
+        #user.birth_date = self.cleaned_data['birth_date']
+        
         user.state = self.cleaned_data['state']
 
         if commit: 
