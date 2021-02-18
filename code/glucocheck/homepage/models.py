c061d976040bf28120fb2a00d4097aa890e8a649
AnjaliSharma2021 <ar_anjali_sharma@yahoo.com>
2021-02-18 18:57:57 -0500

    working on confirmation mail

--- code/glucocheck/homepage/models.py
+++ code/glucocheck/homepage/models.py
@@ -10,10 +10,21 @@ from datetime import date
 class UserProfile(models.Model):
     user = models.OneToOneField(User,on_delete= models.CASCADE)# relationship of user attribute with User model 
     # every profile associated with one user and every user will have one profile
-                                 
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)                        
     birth_date = models.DateField()
     state = models.CharField(max_length=200)
+    signup_confirmation = models.BooleanField(default=False)
+
 
     
     def __str__(self):
         return self.user.username
+
+
+'''@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+'''
