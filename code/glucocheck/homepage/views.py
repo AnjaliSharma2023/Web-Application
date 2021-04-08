549cec1d26a40af6df15699bfce1991c60b4e87a
emnajli <emnajli@my.waketech.edu>
2021-04-08 16:48:41 -0400

    some update

--- code/glucocheck/homepage/views.py
+++ code/glucocheck/homepage/views.py
@@ -305,7 +305,7 @@ def email_input(request):
                 'account_nav': get_account_nav(request.user),
                 'page_title': 'Notice',
                 'message_title': 'Notice',
-                'message_text': [f'Reset password link sent to {user_email}! Please check your email to reset your password.']
+                'message_text': [f'Reset password link sent to {user_email}',' Please check your email to reset your password.']
             }
             return render(request, 'message/message.html', context)
     else:
@@ -344,7 +344,7 @@ def glucose_input(request):
                     'account_nav': get_account_nav(request.user),
                     'page_title': 'Notice',
                     'message_title': 'Notice',
-                    'message_text': ['Value saved.'],
+                    'message_text': ['Glucose value saved'],
                 }
         
             return render(request,'message/message.html', context)
@@ -383,7 +383,7 @@ def carbs_input(request):
                     'account_nav': get_account_nav(request.user),
                     'page_title': 'Notice',
                     'message_title': 'Notice',
-                    'message_text': ['Value saved.'],
+                    'message_text': ['Carbs value saved'],
                 }
         
             return render(request,'message/message.html', context)
@@ -424,7 +424,7 @@ def insulin_input(request):
                     'account_nav': get_account_nav(request.user),
                     'page_title': 'Notice',
                     'message_title': 'Notice',
-                    'message_text': ['Value saved.'],
+                    'message_text': ['Insulin value saved'],
                 }
         
             return render(request,'message/message.html', context)
@@ -691,7 +691,7 @@ def profile_page(request):
                     'account_nav': get_account_nav(request.user),
                     'page_title': 'Notice',
                     'message_title': 'Notice',
-                    'message_text': ['Your profile updated.'],
+                    'message_text': ['Your profile is successfully updated'],
                 }
         
             return render(request,'message/message.html', context)
